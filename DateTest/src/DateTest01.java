0c1ccee9281a8121d5329b513752765616e0243e
xiehairui <18792868985@163.com>
2019-01-28 11:17:05 +0800

    时间类测试

---
+++ DateTest/src/DateTest01.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date();
+
+        System.out.println(date.toString());
+    }
+}
