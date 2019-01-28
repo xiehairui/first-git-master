be56e1dd4fb6ae2a57ea984d70cead89eca742c2
xiehairui <18792868985@163.com>
2019-01-28 11:47:52 +0800

    时间自定义格式

--- DateTest/src/DateTest01.java
+++ DateTest/src/DateTest01.java
@@ -1,9 +1,13 @@
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
 
 public class DateTest01 {
     public static void main(String[] args) {
         Date date = new Date();
-
+        Date date1 = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        System.out.println("当地时间：" + ft.format(date1));
         System.out.println(date.toString());
     }
 }
