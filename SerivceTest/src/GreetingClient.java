591f80f2e9f58872655adfd9374d8b82aa4a5a4f
xiehairui <18792868985@163.com>
2019-02-07 08:48:20 +0800

    服务器通信测试

---
+++ SerivceTest/src/GreetingClient.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.net.Socket;
+
+public class GreetingClient {
+    public static void main(String[] args){
+        String Servername = args[0];
+        int port = Integer.parseInt(args[1]);
+        try{
+            System.out.println("连接到主机："+Servername + ",端口号："+port);
+            Socket client = new Socket(Servername,port);
+            System.out.println("远程主机地址：" +client.getRemoteSocketAddress());
+            OutputStream outToServer = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
+
+            out.writeUTF("Hello from"+ client.getLocalSocketAddress());
+            InputStream inFromServer = client.getInputStream();
+            DataInputStream in = new DataInputStream(inFromServer);
+            System.out.println("服务器响应：" + in.readUTF());
+            client.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
