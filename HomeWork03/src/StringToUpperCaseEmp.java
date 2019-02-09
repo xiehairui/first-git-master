74e05e641c700bc12c3d6536857507d7b2f639bc
xiehairui <18792868985@163.com>
2019-02-09 18:15:46 +0800

    Java 实例 8- 字符串小写转大写

---
+++ HomeWork03/src/StringToUpperCaseEmp.java
@@ -0,0 +1,8 @@
+public class StringToUpperCaseEmp {
+    public static void main(String [] args){
+        String str = " hello wangmin";
+        String strUpper = str.toUpperCase();
+        System.out.println("原始字符：" +str);
+        System.out.println("转化大写后的字符串" + strUpper);
+    }
+}
