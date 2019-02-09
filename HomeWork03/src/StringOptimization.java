38f3908e55d05a8a21954539c7cff13f2e1cec7d
xiehairui <18792868985@163.com>
2019-02-09 20:04:08 +0800

    Java 实例 11- 字符串优化

--- HomeWork03/src/StringOptimization.java
+++ HomeWork03/src/StringOptimization.java
@@ -17,8 +17,8 @@ public class StringOptimization {
             variables[i] = new String("hello");
         }
         long endTime1 = System.currentTimeMillis();
-        //----------------------------------------------
         System.out.println("使用new关键字：" + (endTime1 - startTime1) + "ms");
+        //----------------------------------------------
         long startTime2 = System.currentTimeMillis();
         for(int i = 0; i <50000;i++){
             variables[i] = new String("hello");
