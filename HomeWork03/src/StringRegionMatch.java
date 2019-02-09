f03e9d79e4ac73a05bbfe21ba68723764a9c8e45
xiehairui <18792868985@163.com>
2019-02-09 18:36:57 +0800

    Java 实例 9 - 测试两个字符串区域是否相等

---
+++ HomeWork03/src/StringRegionMatch.java
@@ -0,0 +1,10 @@
+public class StringRegionMatch {
+    public static void main(String [] args){
+        String first_str = "Welcome to Microsoft";
+        String second_str = "I work with microsoft";
+        boolean match1 = first_str.regionMatches(11,second_str,12,9);
+        boolean match2 = first_str.regionMatches(true,11,second_str,12,9);
+        System.out.println("区分大小写"+ match1);
+        System.out.println("不区分大小写" + match2);
+    }
+}
