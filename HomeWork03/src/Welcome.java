08c11504e2763957be7d7e525006262d7384c335
xiehairui <18792868985@163.com>
2019-02-10 09:34:10 +0800

    Java 实例 18- 数组输出

---
+++ HomeWork03/src/Welcome.java
@@ -0,0 +1,11 @@
+public class Welcome {
+    public static void main(String[] args){
+        String [] runoobs =  new String[3];
+        runoobs[0] = "王敏";
+        runoobs[1] = "么么哒";
+        runoobs[2] = "嫁给我啊";
+        for (int i = 0 ; i<runoobs.length;i++){
+            System.out.println(runoobs[i]);
+        }
+    }
+}
