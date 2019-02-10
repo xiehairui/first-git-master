9a3fa2e44925f015addafd196a8f1063cf608613
xiehairui <18792868985@163.com>
2019-02-10 09:17:54 +0800

    Java 实例 17- 数组反转

---
+++ HomeWork03/src/Main4.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Main4 {
+    public static void main(String [] args){
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        arrayList.add("D");
+        arrayList.add("E");
+        System.out.println("反转前排序：" + arrayList);
+        Collections.reverse(arrayList);
+        System.out.println("反转后排序：" +arrayList);
+
+    }
+
+}
