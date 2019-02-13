15c9b3745fd7cd7734ad7e8c924b8cc202e7294a
xiehairui <18792868985@163.com>
2019-02-13 18:35:52 +0800

    Java 实例 43-Break关键字的用法

---
+++ HomeWork03/src/Main16.java
@@ -0,0 +1,20 @@
+public class Main16 {
+    public static void main(String [] args){
+        int[] intary = {99,12,22,34,45,67,5678,8900};
+        int no = 5678;
+        int i = 0;
+        boolean found = false;
+        for(;i<intary.length;i++){
+            if (intary[i] == no){
+                found = true;
+                break;
+            }
+        }
+        if (found){
+            System.out.println(no + "元素的索引位置在：" +i);
+        }
+        else{
+            System.out.println(no + "元素不在数组中");
+        }
+    }
+}
