596a561350e4bd3605dda3030d3754a43ee94ac4
xiehairui <18792868985@163.com>
2019-02-13 20:24:49 +0800

    Java 实例 47- Enum（枚举）构造函数及方法的使用

---
+++ HomeWork03/src/Main20.java
@@ -0,0 +1,23 @@
+public class Main20 {
+    public static void main(String[] args){
+        System.out.println("所有汽车的价格：");
+        for(Car2 c : Car2.values()){
+            System.out.println("需要"+c.getPrice()+"钱");
+        }
+    }
+}
+enum Car2 {
+    lamborghini(900),
+    tata(2),
+    audi(50),
+    fiat(15),
+    honda(12);
+
+    private int price;
+    Car2(int p) {
+        price = p;
+    }
+    int getPrice() {
+        return price;
+    }
+}
