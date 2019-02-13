94ff3559aeb49e285655a6d9dd2e920254634b04
xiehairui <18792868985@163.com>
2019-02-13 18:47:58 +0800

    Java 实例 44- continue 关键字用法

---
+++ HomeWork03/src/Main17.java
@@ -0,0 +1,15 @@
+public class Main17 {
+    public static void main(String [] args){
+        StringBuffer searchstr = new StringBuffer("hello how are you.");
+        int length = searchstr.length();
+        int count = 0;
+        for(int i=0;i<length;i++){
+            if(searchstr.charAt(i) != 'h')
+                continue;
+            count++;
+            searchstr.setCharAt(i,'h');
+        }
+        System.out.println("发现"+count +"个h字符");
+        System.out.println(searchstr);
+    }
+}
