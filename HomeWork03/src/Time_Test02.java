559f9055c57b49ea36e9f75dc44c1f3e1d54d13f
xiehairui <18792868985@163.com>
2019-02-13 11:24:03 +0800

    Java 实例 34- 获取年份、月份等

---
+++ HomeWork03/src/Time_Test02.java
@@ -0,0 +1,21 @@
+import java.util.Calendar;
+
+public class Time_Test02 {
+    public static void main(String [] args){
+        Calendar cal = Calendar.getInstance();
+        int day = cal.get(Calendar.DATE);
+        int month = cal.get(Calendar.MONTH) +1;
+        int year = cal.get(Calendar.YEAR);
+        int dow = cal.get(Calendar.DAY_OF_WEEK);
+        int dom = cal.get(Calendar.DAY_OF_MONTH);
+        int doy = cal.get(Calendar.DAY_OF_YEAR);
+
+        System.out.println("当前时间：" + cal.getTime());
+        System.out.println("日期：" + day);
+        System.out.println("月份：" + month);
+        System.out.println("年份：" + year);
+        System.out.println("一周的第几天：" + dow);//星期日为一周的第一天输出为1，星期一输出为2，以此类推
+        System.out.println("一月中的第几天：" + dom);
+        System.out.println("一年中的第几天：" + doy);
+    }
+}
