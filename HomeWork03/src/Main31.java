65899da6fc26ae1ebfef1b920e947d71539bddcd
xiehairui <18792868985@163.com>
2019-02-14 17:16:49 +0800

    Java 实例 - 检测文件是否存在

---
+++ HomeWork03/src/Main31.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class Main31 {
+    public static void main(String[] args){
+        File file = new File("E:\\first-git-master\\nameTest.txt");
+        System.out.println(file.exists());
+    }
+}
